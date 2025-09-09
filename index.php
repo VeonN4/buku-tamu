baae5f731e5a1aff8b6e0b7adcaea3e1566fc652
VeonN4 <fxzia13@gmail.com>
2025-09-09 14:22:14 +0700

    feat: buku tamu main page

---
+++ index.php
@@ -0,0 +1,17 @@
+<?php
+include "views/header.php";
+include "views/sidebar.php";
+include "views/topbar.php";
+?>
+
+<!-- Begin Page Content -->
+<div class="container-fluid">
+
+    <!-- Page Heading -->
+    <h1 class="h3 mb-4 text-gray-800">Blank Page</h1>
+
+</div>
+
+<?php
+include "views/footer.php";
+?>
