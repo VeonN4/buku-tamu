24573f509a636899d59489f88b51a89d5d95e810
VeonN4 <razfaziya20@gmail.com>
2025-09-11 22:29:25 +0700

    feat: add buku tamu table

---
+++ buku-tamu.php
@@ -0,0 +1,47 @@
+<?php
+include "views/header.php";
+include "views/sidebar.php";
+include "views/topbar.php";
+?>
+
+<!-- Begin Page Content -->
+<div class="container-fluid">
+    <div class="card shadow mb-4">
+        <div class="card-header py-3">
+            <h6 class="m-0 font-weight-bold text-primary">Buku Tamu</h6>
+        </div>
+        <div class="card-body">
+            <div class="table-responsive">
+                <table class="table table-bordered" id="dataTable" width="100%" cellspacing="0">
+                    <thead>
+                        <tr>
+                            <th>ID</th>
+                            <th>Tanggal</th>
+                            <th>Nama Tamu</th>
+                            <th>Alamat</th>
+                            <th>No. HP</th>
+                            <th>Bertemu</th>
+                            <th>Kepentingan</th>
+                        </tr>
+                    </thead>
+                    <tfoot>
+                        <tr>
+                            <th>ID</th>
+                            <th>Tanggal</th>
+                            <th>Nama Tamu</th>
+                            <th>Alamat</th>
+                            <th>No. HP</th>
+                            <th>Bertemu</th>
+                        </tr>
+                    </tfoot>
+                    <tbody>
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    </div>
+</div>
+
+<?php
+include "views/footer.php";
+?>
