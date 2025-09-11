608e8b2d0b4c870a83a0344301ec4dba6b653c07
VeonN4 <fxzia13@gmail.com>
2025-09-12 00:17:21 +0700

    feat(controllers/tamu): added request method check to avoid array undefined warning

--- controllers/TamuControllers.php
+++ controllers/TamuControllers.php
@@ -5,22 +5,26 @@ $action = $_GET['action'] ?? null;
 
 switch ($action) {
     case 'createTamu':
+        echo "Test";
         $prefix = "zt";
         $kode = getTamuLargestId();
 
         $urutan = (int) substr($kode->fetch_assoc()['maxTamuId'], 2, 3);
         $urutan++;
 
-        $kode = $huruf . sprintf("%03s", $urutan);
+        $kode = $prefix . sprintf("%03s", $urutan);
 
-        $kode = htmlspecialchars($kode);
-        $tanggal = date("Y-m-d");
-        $nama_tamu = htmlspecialchars($_POST["nama_tamu"]);
-        $alamat = htmlspecialchars($_POST["alamat"]);
-        $no_hp = htmlspecialchars($_POST["no_hp"]);
-        $bertemu = htmlspecialchars($_POST["bertemu"]);
-        $kepentingan = htmlspecialchars($_POST["kepentingan"]);
+        if ($_SERVER["REQUEST_METHOD"] === "POST") {
+            echo "Test 2";
+            $kode = htmlspecialchars($kode);
+            $tanggal = date("Y-m-d");
+            $nama_tamu = htmlspecialchars($_POST["nama_tamu"]);
+            $alamat = htmlspecialchars($_POST["alamat"]);
+            $no_hp = htmlspecialchars($_POST["no_hp"]);
+            $bertemu = htmlspecialchars($_POST["bertemu"]);
+            $kepentingan = htmlspecialchars($_POST["kepentingan"]);
 
-        createTamu($kode, $tanggal, $nama_tamu, $alamat, $no_hp, $bertemu, $kepentingan);
+            createTamu($kode, $tanggal, $nama_tamu, $alamat, $no_hp, $bertemu, $kepentingan);
+        }
         break;
 }
