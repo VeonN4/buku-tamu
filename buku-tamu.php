4551bb611b68cd8ce1a6efe88cd2196a7bbc8225
VeonN4 <razfaziya20@gmail.com>
2025-09-11 23:00:22 +0700

    feat: show data fetched from db to the table

--- buku-tamu.php
+++ buku-tamu.php
@@ -2,6 +2,8 @@
 include "views/header.php";
 include "views/sidebar.php";
 include "views/topbar.php";
+
+require_once "./models/Tamu.php";
 ?>
 
 <!-- Begin Page Content -->
@@ -32,9 +34,26 @@ include "views/topbar.php";
                             <th>Alamat</th>
                             <th>No. HP</th>
                             <th>Bertemu</th>
+                            <th>Kepentingan</th>
                         </tr>
                     </tfoot>
                     <tbody>
+                        <?php 
+                        $dataTamu = getTamu();
+                        foreach ($dataTamu as $key => $value): 
+                        ?>
+                        <tr>
+                            <td><?= htmlspecialchars($value['id_tamu']) ?></td>
+                            <td><?= htmlspecialchars($value['tanggal']) ?></td>
+                            <td><?= htmlspecialchars($value['nama_tamu']) ?></td>
+                            <td><?= htmlspecialchars($value['alamat']) ?></td>
+                            <td><?= htmlspecialchars($value['no_hp']) ?></td>
+                            <td><?= htmlspecialchars($value['bertemu']) ?></td>
+                            <td><?= htmlspecialchars($value['kepentingan']) ?></td>
+                        </tr>
+                        <?php
+                        endforeach
+                        ?>
                     </tbody>
                 </table>
             </div>
